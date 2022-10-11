7b6b6f60de0dffda39e51b049b0eb132daf2001d
Sakuntha Bimsara <sakuofficial@gmail.com>
2022-10-11 21:44:42 +0530

    add user login req and user info req

--- app/Http/Controllers/Api/PassportAuthController.php
+++ app/Http/Controllers/Api/PassportAuthController.php
@@ -30,4 +30,31 @@ class PassportAuthController extends Controller
   
         return response()->json(['token' => $token], 200);
     }
+
+     /**
+     * Login Req
+     */
+    public function login(Request $request)
+    {
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+  
+        if (auth()->attempt($data)) {
+            $token = auth()->user()->createToken('Laravel8PassportAuth')->accessToken;
+            return response()->json(['token' => $token], 200);
+        } else {
+            return response()->json(['error' => 'Unauthorised'], 401);
+        }
+    }
+
+    public function userInfo() 
+    {
+ 
+     $user = auth()->user();
+      
+     return response()->json(['user' => $user], 200);
+ 
+    }
 }
