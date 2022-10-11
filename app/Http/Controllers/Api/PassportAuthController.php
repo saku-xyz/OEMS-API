04c94b03741e2ae46cfa66a427aba046044e4003
Sakuntha Bimsara <sakuofficial@gmail.com>
2022-10-11 21:43:00 +0530

    add user registration function

---
+++ app/Http/Controllers/Api/PassportAuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\User;
+
+class PassportAuthController extends Controller
+{
+    /**
+     * Registration Req
+     */
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|min:4',
+            'email' => 'required|email',
+            'password' => 'required|min:8',
+        ]);
+  
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+  
+        $token = $user->createToken('Laravel8PassportAuth')->accessToken;
+  
+        return response()->json(['token' => $token], 200);
+    }
+}
