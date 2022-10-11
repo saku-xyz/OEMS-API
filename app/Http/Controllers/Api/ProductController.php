112827a055b026e03703c781cd5a2a3eb449e290
Sakuntha Bimsara <sakuofficial@gmail.com>
2022-10-11 21:50:23 +0530

    fet all product api

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -1,11 +1,30 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Validator;
 
 class ProductController extends Controller
 {
-    //
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function index()
+    {
+        $products = Product::all();
+        return response()->json([
+            "success" => true,
+            "message" => "Product List",
+            "data" => $products
+        ]);
+    }
+
+  
 }
